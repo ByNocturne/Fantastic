92d25d14bb1b5d830468d359680e12c1c1072ee7
DaltonLF <daltonferreiral@outlook.com>
2016-09-10 19:00:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function addBola() {
+	var bola = document.createElement("div");
+	bola.setAttribute("class", "bola");
+	var p1 = Math.floor(Math.random() * 500);
+	var p2 = Math.floor(Math.random() * 400);
+	bola.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;")
+	bola.setAttribute("onclick", "estourar(this)");
+
+	document.body.appendChild(bola);
+}
+function estourar(elemento) {
+	document.body.removeChild(elemento);
+}
+function iniciar() {
+	setInterval(addBola,1000);
+}
